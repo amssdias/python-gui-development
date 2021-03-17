0ffa558ffdfd96a06d49996a78327e6fd85db575
André Dias <andremssdias@hotmail.com>
2021-03-17 15:15:31 +0100

    Creating notebook

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tkinter
+
+Tkinter (tee-kay-in-ter) is a toolkit for creating graphical user interfaces with Python. It's by no means the only one available, and we also have options like Kivy or PyQt. Tkinter is, however, something of a default option for creating GUIs with Python, and actually comes with most Python installations.
+
+## Geometry manager
+
+When we go about creating a GUI with Tkinter, we start with a window, or multiple windows, and inside this windows live a number of widgets. A widget is something like a component. Together these widgets, or components, make up our complete application. We might have buttons, and containers, text areas, and menus. These are all widgets.
+
+The geometry managers are responsible for controlling how these widgets fit into our application windows, and where these widgets are ultimately placed.
+
+Tkinter has a number of geometry managers which behave in different ways. Here we'll be focusing on the pack geometry manager, which is really best for simpler applications, where the interface isn't particularly complicated.
+
+While pack is quite easy to get started with, it can sometimes be quite difficult to visualise how elements are going to fit together, which is why we probably wouldn't use it for a complex interface.
--- app.py
+++ app.py
@@ -1,23 +1,56 @@
+import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
-def greet():
-    print(f"Hello, {user_name.get() or 'World'}")
+def create_file():
+    text_area = tk.Text(notebook)
+    text_area.pack(fill="both", expand=True)
+    notebook.add(text_area, text="Untitled")
+    notebook.select(text_area)
+
+
+def save_file():
+    # Get the path of where the file will be saved
+    file_path = filedialog.asksaveasfilename()
+
+    try:
+        filename = os.path.basename(file_path)
+        text_widget = root.nametowidget(notebook.select())
+        content = text_widget.get("1.0", "end-1c")
+
+        with open(file_path) as file_:
+            file_.write(content)
+
+    except (AttributeError, FileNotFoundError):
+        print("Save operation cancelled")
+        return
+
+    notebook.tab("current", text=filename)
 
 
 root = tk.Tk()
-root.title("Greeter")
+root.title('Teclado Text Editor')
+root.option_add("*tearOFF", False)
 
-user_name = tk.StringVar()
+# main goes inside root
+main = ttk.Frame(root)
+main.pack(fill="both", expand=True, padx=50, pady=(4, 0))
 
-name_label = ttk.Label(root, text='Name: ')
-name_label.pack(side='left', padx=(0, 10))
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
-name_entry.pack(side='left')
-name_entry.focus()
+# creating the menu bar
+menubar = tk.Menu()
+root.config(menu=menubar)
 
-greet_button = ttk.Button(root, text='Greet', command=greet)
-greet_button.pack(side='left', fill='x', expand=True)
+# Create element and put inside the menu bar
+file_menu = tk.Menu(menubar)
+menubar.add_cascade(menu=file_menu, label="File")
+
+# Create the cascade for the element in the menu bar with commands
+file_menu.add_command(label='New', command=create_file)
+file_menu.add_command(label='Save', command=save_file)
+
+# Create notebook and put it inside main
+notebook = ttk.Notebook(main)
+notebook.pack(fill="both", expand=True)
 
 root.mainloop()
