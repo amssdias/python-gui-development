47bde1787c958c2d41a4c88ffc3dd89059e0d83a
André Dias <andremssdias@hotmail.com>
2021-03-19 00:55:24 +0100

    Notebook finished

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 
+# Get info for save or unsaved tabs
 text_contents = dict()
 
 def create_file(content="", title="Untitled"):
-    text_area = tk.Text(notebook)
-    text_area.insert("end", content)
-    text_area.pack(fill="both", expand=True)
+    container = ttk.Frame(notebook)
+    container.pack()
 
-    notebook.add(text_area, text=title)
-    notebook.select(text_area)
+    text_area = tk.Text(container)
+    text_area.insert("end", content)
+    text_area.pack(side="left", fill="both", expand=True)
+
+    notebook.add(container, text=title)
+    notebook.select(container)
+
+    text_scroll = ttk.Scrollbar(container, orient="vertical", command=text_area.yview)
+    text_scroll.pack(side="right", fill="y")
+    text_area["vscrollcommand"] = text_scroll.set
 
     text_contents[str(text_area)] = hash(content)
 
@@ -28,8 +36,55 @@ def check_for_changes():
 
 
 def get_text_widget():
-    text_widget = root.nametowidget(notebook.select())
+    tab_widget = root.nametowidget(notebook.select())
+    text_widget = tab_widget.winfo_children()[0]
     return text_widget
+
+
+def close_current_tab():
+    current = get_text_widget()
+    if current_tab_unsaved() and not confirm_close():
+        return
+    
+    if len(notebook.tabs()) == 1:
+        create_file()
+
+    notebook.forget(current)
+
+
+def current_tab_unsaved():
+    text_widget = get_text_widget()
+    content = text_widget.get("1.0", "end-1c")
+    return hash(content != text_contents[str(text_widget)])
+
+
+def confirm_close():
+    return messagebox.askyesno(
+        message="You have unsaved changes. Are you sure you want to quit?",
+        icon="question",
+        title="Unsaved Changes" 
+    )
+
+
+def confirm_quit():
+    unsaved = False
+
+    # Loop thru all tabs
+    for tab in notebook.tabs():
+        tab_widget = root.nametowidget(tab)
+        text_widget = tab_widget.winfo_children()[0]
+        content = text_widget.get("1.0", "end-1c")
+
+        # If one tab content is not equal to dict, then break
+        if hash(content) != text_contents[str(text_widget)]:
+            unsaved = True
+            break
+
+    # Ask user if they really want to close
+    if unsaved and not confirm_close():
+        return
+
+        root.destroy()
 
 
 def save_file():
@@ -47,7 +102,7 @@ def save_file():
         print("Save operation cancelled")
         return
 
-    notebook.tab("current", text=filename)
+    notebook.tab("current", text=filename) # Change name of current widget
     text_contents[str(text_widget)] = hash(content)
 
 
@@ -67,6 +122,13 @@ def open_file():
     create_file(content, filename)
 
 
+def show_about_info():
+    messagebox.showinfo(
+        title="About",
+        message="The Teclado Text Editor is a simple tabbed text editor designed to help you learn Tkinter."
+    )
+
+
 root = tk.Tk()
 root.title('Teclado Text Editor')
 root.option_add("*tearOFF", False)
@@ -81,12 +143,19 @@ root.config(menu=menubar)
 
 # Create element and put inside the menu bar
 file_menu = tk.Menu(menubar)
+help_menu = tk.Menu(menubar)
+
 menubar.add_cascade(menu=file_menu, label="File")
+menubar.add_cascade(menu=help_menu, label="Help")
 
 # Create the cascade for the element in the menu bar with commands
 file_menu.add_command(label='New', command=create_file, accelerator="Ctrl+N")
 file_menu.add_command(label='Open...', command=open_file, accelerator="Ctrl+O")
 file_menu.add_command(label='Save', command=save_file, accelerator="Ctrl+S")
+file_menu.add_command(label='Close Tab', command=close_current_tab, accelerator="Ctrl+Q")
+file_menu.add_command(label='Exit', command=confirm_quit)
+
+help_menu.add_command(label="About", command=show_about_info)
 
 # Create notebook and put it inside main
 notebook = ttk.Notebook(main)
@@ -97,6 +166,7 @@ create_file()
 root.bind("<KeyPress>", lambda event: check_for_changes())
 root.bind("<Control-n>", lambda event: create_file())
 root.bind("<Control-o>", lambda event: open_file())
+root.bind("<Control-q>", lambda event: close_current_tab())
 root.bind("<Control-s>", lambda event: save_file())
 
 root.mainloop()
